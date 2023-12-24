c889a6d88a7182834c47bf3bd18a61e2c4c8a199
ajinkyal3061 <ajinkyalandge401@gmail.com>
2023-12-24 13:22:49 +0530

    Java Code first commit

---
+++ src/javaSessions/CallByRef.java
@@ -0,0 +1,20 @@
+package javaSessions;
+
+public class CallByRef {
+
+	public static void sendMail(CallByRef a) {
+		System.out.println("send mail");
+		a.readMail();
+	}
+
+	public void readMail() {
+		System.out.println("read mail");
+	}
+
+	public static void main(String[] args) {
+
+		CallByRef obj = new CallByRef();
+		CallByRef.sendMail(obj);
+	}
+
+}
---
+++ src/javaSessions/FirstJavaCode.java
@@ -0,0 +1,12 @@
+package javaSessions;
+
+public class FirstJavaCode {
+
+	public static void main(String[] args) {
+		
+		String A = "Hello World";
+		System.out.println(A);
+
+	}
+
+}
