b976d51bcc5602aa7cd16202d35f54d03c442715
ajinkyal3061 <ajinkyalandge401@gmail.com>
2023-12-26 18:00:22 +0530

    java code

---
+++ src/OOP_Interface/IndianMedical.java
@@ -0,0 +1,10 @@
+package OOP_Interface;
+
+public interface IndianMedical {
+
+	public void gynoServices();
+
+	public void ENTServices();
+
+	public void emergencyServices();
+}
---
+++ src/WebDriverArch/Testing.java
@@ -0,0 +1,8 @@
+package WebDriverArch;
+
+public class Testing {
+
+	private Testing() {
+	}
+	// what kind of methods? ---> Static Methods
+}
